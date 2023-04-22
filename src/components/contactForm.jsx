1a10d85870f7b285697b5e60e8adee8b4bae34c5
gabess3 <95196142+gabess3@users.noreply.github.com>
2023-04-21 21:05:41 -0500

    updated contact form so that the validators for the name, email, and message forms now run properly on submit and throw alerts if invalid or incomplete information is submitted

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -22,35 +22,57 @@ export default function ContactForm() {
     setEmail(emailInput.value);
     const emailWarning = document.getElementById('emailRequired');
     if(validateEmail(email)) {
-      console.log('yay')
+      console.log('yay email')
       emailWarning.textContent = '';
-    } else 
+    } else  {
+      console.log('bad email')
       emailWarning.setAttribute('class', 'contactAlert')
       emailWarning.textContent = "*This is a required field. Please enter a valid email address.";
+      }
     };
+
+  const changeEmail = (event) => {
+    setEmail(event.target.value)
+    console.log(email)
+  }
 
   const handleName = () => {
     const nameInput = document.getElementById('name');
     setName(nameInput.value)
-    console.log(name)
     const nameWarning = document.getElementById('nameRequired')
     if (name.length>0) {
+      console.log('yay name')
       nameWarning.textContent = '';
-    } else if (name.length===0)
+    } else if (name.length===0) {
+    console.log('bad name')
     nameWarning.setAttribute('class', 'contactAlert')
     nameWarning.textContent = "*This is a required field. Please enter your name."
+    }
   };
+
+  const changeName = (event) => {
+    setName(event.target.value)
+    console.log(name)
+  }
 
   const handleMessage = () => {
     const messageInput = document.getElementById('standard-multiline-flexible')
     setMessage(messageInput.value);
     const messageWarning = document.getElementById('messageRequired')
     if (message.length > 0) {
+      console.log('yay message')
       messageWarning.textContent = '';
-    } else if (message.length===0)
+    } else if (message.length===0) {
+      console.log('bad message')
       messageWarning.setAttribute('class', 'contactAlert')
       messageWarning.textContent = "*This is a required field. Please enter a message to send in your email."
+    }
   };
+
+  const changeMessage = (event) => {
+    setMessage(event.target.value)
+    console.log(message)
+  }
 
   const submitHandler = () => {
     handleEmail();
@@ -64,12 +86,12 @@ export default function ContactForm() {
       <h3>Let's Work Together!</h3>
       <FormControl sx={{ width: 300, m:5 }}>
         <InputLabel htmlFor="name" required={true}>Name</InputLabel>
-        <Input id="name" aria-describedby="my-helper-name"/>
-        <div id="nameRequired"></div>
+        <Input id="name" aria-describedby="my-helper-name" value={name} onChange={changeName}/>
+        <p id="nameRequired"></p>
       </FormControl>
       <FormControl sx={{ width: 300, m:5 }}>
-        <InputLabel htmlFor="my-input" required={true}>Email address</InputLabel>
-        <Input id="email" aria-describedby="my-helper-text" onChange={handleEmail} />
+        <InputLabel htmlFor="my-input"  required={true}>Email address</InputLabel>
+        <Input id="email" aria-describedby="my-helper-text" value={email} onChange={changeEmail}/>
         <p id='emailRequired'></p>
       </FormControl>
       <TextField
@@ -80,6 +102,8 @@ export default function ContactForm() {
           variant="filled"
           minRows={5}
           required={true}
+          value={message}
+          onChange={changeMessage}
         />
         <p id='messageRequired'></p>
       <Grid align="right">                    
