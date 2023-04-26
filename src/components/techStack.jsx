e22820776a84c9e6157651adbc6d1d13e6b302a1
gabess3 <95196142+gabess3@users.noreply.github.com>
2023-04-26 14:53:07 -0500

    updated last minute stylings for tech stack

--- src/components/techStack.jsx
+++ src/components/techStack.jsx
@@ -28,57 +28,56 @@ export default function TechStack() {
   ]
 
   return (
-    <div className='techContainer'>
-    <FadeInSection>
-    <h2 className='techStack'>/techStack</h2>
-    </FadeInSection>
     <div className='tech'>
 
       <div className='frontEnd'>
-      <FadeInSection>
-        <h2>FRONT END TECHNOLOGIES</h2>
-      </FadeInSection>
-      <FadeInSection>
-        <ul className='list'>
-          {frontEnd.map((tech) =>
-            <li key={tech}>{tech}</li>
-          )}
-        </ul>
+        <FadeInSection>
+          <h2>FRONT END TECHNOLOGIES</h2>
+        </FadeInSection>
+        <FadeInSection>
+          <ul className='list'>
+            {frontEnd.map((tech) =>
+              <li key={tech}>{tech}</li>
+            )}
+          </ul>
         </FadeInSection>
       </div>
+
       <div className='backEnd'>
-      <FadeInSection>
-        <h2>BACK END TECHNOLOGIES</h2>
-      </FadeInSection>
-      <FadeInSection>
-        <ul className='list'>
-          {backEnd.map((tech) =>
-            <li key={tech}>{tech}</li>
-          )}
-        </ul>
-      </FadeInSection>
+        <FadeInSection>
+          <h2>BACK END TECHNOLOGIES</h2>
+        </FadeInSection>
+        <FadeInSection>
+          <ul className='list'>
+            {backEnd.map((tech) =>
+              <li key={tech}>{tech}</li>
+            )}
+          </ul>
+        </FadeInSection>
       </div>
+
       <FadeInSection>
-      <Paper elevation={8}>
-        <div className='icons'>
-          <img src={HtmlIcon} alt="html icon" width='40px' height="40px"></img>
-          <img src={CssIcon} alt="css icon" width='40px' height="40px"></img>
-          <img src={JavascriptIcon} alt="javascript icon" width='40px' height="40px"></img>
-          <img src={reactIcon} alt="react icon" width='40px' height="40px"></img>
-        </div>
-      </Paper>
+        <Paper elevation={8}>
+          <div className='icons'>
+            <img src={HtmlIcon} alt="html icon" width='40px' height="40px"></img>
+            <img src={CssIcon} alt="css icon" width='40px' height="40px"></img>
+            <img src={JavascriptIcon} alt="javascript icon" width='40px' height="40px"></img>
+            <img src={reactIcon} alt="react icon" width='40px' height="40px"></img>
+          </div>
+        </Paper>
       </FadeInSection>
+
       <FadeInSection>
-      <Paper elevation={8}>
-        <div className='icons'>
-          <img src={nodeIcon} alt="html icon" width='40px' height="40px"></img>
-          <img src={expressIcon} alt="javascript icon" width='40px' height="40px" style={{backgroundColor: 'white'}}></img>
-          <img src={mysqlIcon} alt="javascript icon" width='40px' height="40px"></img>
-          <img src={mongodbIcon} alt="css icon" width='40px' height="40px"></img>
-        </div>
-      </Paper>
+        <Paper elevation={8}>
+          <div className='icons'>
+            <img src={nodeIcon} alt="html icon" width='40px' height="40px"></img>
+            <img src={expressIcon} alt="javascript icon" width='40px' height="40px" style={{backgroundColor: 'white'}}></img>
+            <img src={mysqlIcon} alt="javascript icon" width='40px' height="40px"></img>
+            <img src={mongodbIcon} alt="css icon" width='40px' height="40px"></img>
+          </div>
+        </Paper>
       </FadeInSection>
     </div>
-    </div>
+
   )
 }
