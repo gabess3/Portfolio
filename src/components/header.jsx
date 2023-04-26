f1e57e3fc063710228a8974a5c4dcccddcdddd01
gabess3 <95196142+gabess3@users.noreply.github.com>
2023-04-26 16:38:37 -0500

    updated about me section content

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
 									<div className='about container'>
 										<h1>GABRIEL ESS</h1>
 										<h3>FULL STACK DEVELOPER</h3>
-										<p>Some text briefly about me and some whys. I might say a few things about me, music, and perhaps philosophy and my background in tech, but mostly it'll be relevant to being a developer. Now I'm just going to fill the space with as much text as possible so I can see what it'll look like with a more fleshed out bio sitting there. I don't know, maybe we'll talk about farm animals, the gas prices, or the weather. Maybe Hamlet. Poor Yorick? Did anyone actually know him? I doubt it.</p>
+										<p>Hello! My name is Gabriel Ess, and I'm a Dallas-based Full Stack developer. I have a background in Tech Support, Music Education, and Philosophy, all of which lend their own unique qualities in my approach and perspective when developing web applications, such as: an attention to detail, research, technical communication, creativity, and collaboration. I have a life-long passion for continued learning, and bettering myself in every way. Let's work together!</p>
 									</div>
 							</div>
 					</header>
