df0f9eab5b0e65364312a6ff62d11f6452a66c83
gabess3 <95196142+gabess3@users.noreply.github.com>
2023-06-18 13:15:29 -0500

    updated projects section

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,102 +1,102 @@
-import * as React from "react";
+import * as React from 'react';
 import {
-  Button,
-  CardActionArea,
-  CardActions,
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-} from "@mui/material";
-import NoShelfControl from "../images/NoShelfControl.png";
-import socialNetwork from "../images/socialNetwork.png";
-import fish from "../images/fish.png";
-import FadeInSection from "./fadeIn";
+	Button,
+	CardActionArea,
+	CardActions,
+	Card,
+	CardMedia,
+	CardContent,
+	Typography,
+} from '@mui/material';
+import NoShelfControl from '../images/NoShelfControl.png';
+import socialNetwork from '../images/socialNetwork.png';
+import BarbePortraits from '../images/BarbePortraits.png';
+import FadeInSection from './fadeIn';
 
 export default function Project() {
-  const projects = [
-    {
-      name: "FISH TRACKER",
-      link: "https://github.com/NTumminaro/ACGC-Golden-Rod-Pro",
-      image: fish,
-      description:
-        "This is a project that tracks the location of Fish in the GameCube game Animal Crossing. This information is used for Speedrunners in the gaming community. This was created with React / Vite.",
-    },
-    {
-      name: "SOCIAL NETWORK API",
-      link: "https://github.com/gabess3/Social-Network-API",
-      image: socialNetwork,
-      description:
-        "This is an API CRUD application made with MongoDB that retrieves information used on a social network such as user information, thoughts, reactions, etc.",
-    },
-    {
-      name: "NO SHELF CONTROL",
-      link: "https://github.com/Nick-likes-Rust-and-Next/No-Shelf-Control",
-      image: NoShelfControl,
-      description:
-        "This is a MERN stack application that used Apollo/GraphQL and Material UI.",
-    },
-  ];
+	const projects = [
+		{
+			name: 'Barbe Portraits',
+			link: 'https://github.com/gabess3/BarbePortraits',
+			image: BarbePortraits,
+			description:
+				'This project was built with React + Vite, and Material UI. It serves as a landing page for an artist to host examples of her work and commissions, methods of contact, and different options / prices to select from. ',
+		},
+		{
+			name: 'SOCIAL NETWORK API',
+			link: 'https://github.com/gabess3/Social-Network-API',
+			image: socialNetwork,
+			description:
+				'This is an API CRUD application made with MongoDB that retrieves information used on a social network such as user information, thoughts, reactions, etc.',
+		},
+		{
+			name: 'NO SHELF CONTROL',
+			link: 'https://github.com/Nick-likes-Rust-and-Next/No-Shelf-Control',
+			image: NoShelfControl,
+			description:
+				'This is a MERN stack application that used Apollo/GraphQL and Material UI.',
+		},
+	];
 
-  return (
-    <div className="projectContainer">
-      <div id="projects">
-        <FadeInSection>
-          <h2 style={{ fontFamily: "Poppins", fontSize: 40, marginTop: 100 }}>
-            /projects
-          </h2>
-        </FadeInSection>
-      </div>
-      <div className="projects">
-        {projects.map((project) => (
-          <FadeInSection key={project.name}>
-            <Card
-              key={project.name}
-              sx={{ maxWidth: 345, maxHeight: 420, m: 8 }}
-            >
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="160"
-                  image={project.image}
-                  alt={project.name}
-                />
-                <CardContent>
-                  <Typography
-                    sx={{ fontFamily: "Poppins", textAlign: "center" }}
-                    gutterBottom
-                    variant="h5"
-                    component="div"
-                  >
-                    {project.name}
-                  </Typography>
-                  <Typography
-                    sx={{
-                      fontFamily: "Poppins",
-                      textAlign: "center",
-                      fontSize: 12,
-                    }}
-                    variant="body2"
-                    color="text.secondary"
-                  >
-                    {project.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions className="buttonCenter">
-                <Button
-                  href={project.link}
-                  target="_blank"
-                  size="small"
-                  color="primary"
-                >
-                  Open in GitHub
-                </Button>
-              </CardActions>
-            </Card>
-          </FadeInSection>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className='projectContainer'>
+			<div id='projects'>
+				<FadeInSection>
+					<h2 style={{ fontFamily: 'Poppins', fontSize: 40, marginTop: 100 }}>
+						/projects
+					</h2>
+				</FadeInSection>
+			</div>
+			<div className='projects'>
+				{projects.map((project) => (
+					<FadeInSection key={project.name}>
+						<Card
+							key={project.name}
+							sx={{ maxWidth: 345, maxHeight: 420, m: 8 }}
+						>
+							<CardActionArea>
+								<CardMedia
+									component='img'
+									height='160'
+									image={project.image}
+									alt={project.name}
+								/>
+								<CardContent>
+									<Typography
+										sx={{ fontFamily: 'Poppins', textAlign: 'center' }}
+										gutterBottom
+										variant='h5'
+										component='div'
+									>
+										{project.name}
+									</Typography>
+									<Typography
+										sx={{
+											fontFamily: 'Poppins',
+											textAlign: 'center',
+											fontSize: 12,
+										}}
+										variant='body2'
+										color='text.secondary'
+									>
+										{project.description}
+									</Typography>
+								</CardContent>
+							</CardActionArea>
+							<CardActions className='buttonCenter'>
+								<Button
+									href={project.link}
+									target='_blank'
+									size='small'
+									color='primary'
+								>
+									Open in GitHub
+								</Button>
+							</CardActions>
+						</Card>
+					</FadeInSection>
+				))}
+			</div>
+		</div>
+	);
 }
